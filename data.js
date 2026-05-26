9297f66cd19c931fe4b4d552e9c1a484f7619bd7
Christian Martin <167330122+ChrisM7896@users.noreply.github.com>
2026-05-26 18:48:15 -0400

    Testing twitch embed

--- data.js
+++ data.js
@@ -18,6 +18,11 @@ function Record(name, attempts, enjoyment, opinion, proof) {
     this.proof = proof;
 }
 
+// Embed formats
+// YOUTUBE: https://youtube.com/embed/VIDEO_CODE
+// MEDAL: https://medal.tv/games/geometry-dash/clip/VIDEO CODE
+// STREAMABLE: https://www.streamable.com/e/VIDEO_CODE
+
 let levels = [
     /*
     ....###....##.....##.########.########.##.....##.##....##..######..########
@@ -28,7 +33,7 @@ let levels = [
     .##.....##.##.....##.##..........##....##.....##....##....##....##....##...
     .##.....##.##.....##.########....##....##.....##....##.....######.....##...
     */
-
+    new Level("test", 110, "Amethyst", "140134987", "Any Route", "Farlayy", "EndGamer7896", "https://clips.twitch.tv/embed?clip=RamshackleOddWoodpeckerPrimeMe-AZ9cB_7L3ZPgtDu5&parent=https://robotchallengelist.github.io", []),
     new Level("Mad Prophets", 103, "Amethyst", "140134987", "Any Route", "Farlayy", "EndGamer7896", "https://medal.tv/games/geometry-dash/clip/mGyEUN2HMTp9yPEph?invite=cr-MSxscmEsNTk1MDYwNDc2", [
         new Record("EndGamer7896", "13,574", "8/10", "Low Amethyst, 100-105 points", "https://medal.tv/games/geometry-dash/clips/mGyEUN2HMTp9yPEph?invite=cr-MSxscmEsNTk1MDYwNDc2"),
         new Record("Beefyikes", "4,500", "6/10", "Probably top 1", "https://youtu.be/LzfdYC6YebQ"),
